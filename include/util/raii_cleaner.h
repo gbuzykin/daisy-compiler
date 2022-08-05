9901b11e689d1f3167d51f73b05db9f109361184
Georgii Buzykin <gbuzykin@yandex.ru>
2022-08-05 22:35:32 +0300

    Introduce `util::raii_cleaner<>` helper template class

---
+++ include/util/raii_cleaner.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <tuple>
+
+namespace util {
+template<typename FnTy>
+class raii_cleaner {
+ public:
+    explicit raii_cleaner(FnTy fn) : clean_fn_(fn) {}
+    ~raii_cleaner() { clean_fn_(); }
+
+ private:
+    FnTy clean_fn_;
+};
+template<typename... Ts>
+class raii_cleaner<std::tuple<Ts&...>> {
+    static_assert(sizeof...(Ts) != 0, "at least one parameter must be specified");
+
+ public:
+    explicit raii_cleaner(std::tuple<Ts&...> v) : v_(v) {}
+    ~raii_cleaner() { clean_impl(std::index_sequence_for<Ts...>()); }
+
+ private:
+    std::tuple<Ts&...> v_;
+
+    template<size_t... Is>
+    void clean_impl(std::index_sequence<Is...>) {
+        (std::get<Is>(v_).clear(), ...);
+    }
+};
+template<typename Ty>
+raii_cleaner(Ty) -> raii_cleaner<Ty>;
+}  // namespace util
